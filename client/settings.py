da1847bb60792a3d803ef41b2398f9fdee891aba
Meenu Venkittarajan <vpmeenu@gmail.com>
2015-05-22 17:54:12 -0400

    Fixing config stuff

--- client/settings.py
+++ client/settings.py
@@ -8,10 +8,10 @@
 #     http://doc.scrapy.org/en/latest/topics/settings.html
 #
 
-BOT_NAME = 'tutorial'
+BOT_NAME = 'client'
 
-SPIDER_MODULES = ['tutorial.spiders']
-NEWSPIDER_MODULE = 'tutorial.spiders'
+SPIDER_MODULES = ['client.spiders']
+NEWSPIDER_MODULE = 'client.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'tutorial (+http://www.yourdomain.com)'
