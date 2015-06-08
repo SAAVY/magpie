92b2b82c2aaa3d44ed0fdb49f24b9d32b7747996
Samiya Akhtar <samiyaakhtar7@gmail.com>
2015-06-08 00:26:12 -0400

    Some initial code for dropbox and wikipedia

---
+++ client/dropbox_scraper.py
@@ -0,0 +1,31 @@
+import dropbox
+import urllib2
+
+app_key = '0httdxchkqzhydp'
+app_secret = 'kzycvt4fb0979qa'
+
+
+def scrape(url):
+    flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
+
+    # Have the user sign in and authorize this token
+    authorize_url = flow.start()
+    print '1. Go to: ' + authorize_url
+    print '2. Click "Allow" (you might have to log in first)'
+    print '3. Copy the authorization code.'
+    code = raw_input("Enter the authorization code here: ").strip()
+    # KDPt2zypu5sAAAAAAAADxqLDHeyzHmUVCAGpEvKXYkA
+    # This will fail if the user enters an invalid authorization code
+    access_token, user_id = flow.finish(code)
+
+    client = dropbox.client.DropboxClient(access_token)
+    print 'linked account: ', client.account_info()
+
+    out = open('magnum-opus.txt', 'wb')
+    f, metadata = client.get_file_and_metadata(url)
+    with f:
+        out.write(f.read())
+    print metadata
+
+if __name__ == "__main__":
+    scrape("/s/vzk05zglkqdcqci/mscinotes.pdf")
---
+++ client/wikipedia_scraper.py
@@ -0,0 +1,17 @@
+import wikipedia
+import lxml.html
+
+
+def scrape(url):
+    t = lxml.html.parse(url)
+    title = t.find(".//title").text
+
+    page = wikipedia.page(title)
+    print page.summary
+    print "Image: {0}".format(page.images[0])
+    print "Title: {0}".format(title)
+
+
+
+if __name__ == "__main__":
+    scrape("http://en.wikipedia.org/wiki/Toronto")
