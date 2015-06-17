3472116d2809ea4f9da9da470b723f7eb89f143d
Meenu Venkittarajan <vpmeenu@gmail.com>
2015-06-17 12:58:08 -0400

    Specific wikipedia case - adding to existing parsing stuff

--- client/api.py
+++ client/api.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from scraper import scraper
+from scraper import scraper, wikipedia_scraper
 import json
 app = Flask(__name__)
 
@@ -9,10 +9,15 @@ def hello_world():
 
 @app.route('/website', methods=['GET'])
 def get_metadata():
-    local_scraper = scraper.Scraper(url=request.args.get('src'))
-    prop_map = local_scraper.scrape_website()
-    json_return = json.dumps(prop_map)
-    return json_return
+    src_url = request.args.get('src')
+    if "wikipedia" in src_url:
+        wikipedia_scraper.scrape(url=src_url)
+        return ""
+    else:
+        local_scraper = scraper.Scraper(url=request.args.get('src'))
+        prop_map = local_scraper.scrape_website()
+        json_return = json.dumps(prop_map)
+        return json_return
 
 if __name__ == '__main__':
     app.run(debug=True)
