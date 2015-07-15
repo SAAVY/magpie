247c0f5ffcbfd463f8c811003dba8d82871b5483
AliceYuan <yuan.alice93@gmail.com>
2015-07-14 23:36:10 -0400

    minor fix to build/install script & python path in make file

--- install.sh
+++ install.sh
@@ -7,6 +7,13 @@ BLUE='\033[34m'
 BOLD='\033[1m'
 NC='\033[0m' # No Color
 
+echo -e "${BOLD}================== CHECK VIRTUAL ENV ====================${NC}"
+python -c 'import sys; print sys.real_prefix' 2>/dev/null && INVENV=1 || INVENV=0
+if [ $INVENV -eq 0 ]; then
+	echo -e "${RED}ERROR: Not in virtual env! Source env by doing . venv/bin/activate${NC}";
+	exit 1;
+fi
+
 echo -e "${BOLD}================== INSTALLING REQUIREMENTS ====================${NC}"
 pip install -r requirements.txt
 echo -e "${BOLD}================== END INSTALL REQUIREMENTS ====================${NC}"
