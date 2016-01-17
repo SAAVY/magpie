a273544cc726796eba8fec5a4a8b831b286062a1
AliceYuan <yuan.alice93@gmail.com>
2016-01-17 13:24:58 -0500

    Implement logging to file

--- client/api_handler.py
+++ client/api_handler.py
@@ -1,5 +1,6 @@
-import config
-import cache_utils
+import json
+import logging
+
 from constants import ResponseType
 from constants import StatusCode
 from constants import UrlTypes
@@ -13,13 +14,20 @@ from scraper import wikipedia_metadata
 from scraper import youtube_metadata
 import url_utils
 from utils.profile import cprofile
+from flask import current_app
 
+import config
+import cache_utils
 from flask import Response as FlaskResponse
 
 
 @cprofile
-def get_metadata(url, response_type):
+def get_metadata(url, response_type=ResponseType.JSON):
+    logger = current_app.logger
+    logger.setLevel(logging.DEBUG)
+    logger.debug("FUNC: get_metadata, URL: %s" % url)
     sanitized_url = url_utils.sanitize_url(url)
+    logger.debug("Sanitized url: %s" % sanitized_url)
 
     head = url_utils.get_requests_header(sanitized_url)
     if head is None:
@@ -33,14 +41,18 @@ def get_metadata(url, response_type):
     if config.CACHE_DATA:
         data = cache_utils.get_cached_data(sanitized_url)   # If data from db is None, continue and parse the website
         if data is not None:
+            logger.debug("Cache hit for key %s", sanitized_url)
             return data.metadata
     # return response based on response type
     if response_type == ResponseType.JSON:
         json_data = get_json_metadata(metadata)
+        logger.info(json.dumps(json_data))
         if config.CACHE_DATA:
+            logger.debug("Caching json data to redis db")
             cache_utils.cache_json_data(sanitized_url, json_data)
         response = FlaskResponse(response=json_data, status=StatusCode.OK, mimetype="application/json")
         return response
+    return None
 
 
 def get_json_metadata(metadata):
